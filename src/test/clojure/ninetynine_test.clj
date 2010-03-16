aa2b781fc3ff02533cb92b5703d0b73d82b7d692
Alf Kristian Stoyle <alf.kristian@gmail.com>
2010-03-17 00:12:42 +0100

    Testing mylength.

--- src/test/clojure/ninetynine_test.clj
+++ src/test/clojure/ninetynine_test.clj
@@ -1,11 +1,10 @@
 (ns ninetynine_test
   (:use ninetynine clojure.test))
 
-(defn- exec-last [f]
-  (is (= 8 (f '(1, 1, 2, 3, 5, 8))))
-  (is (nil? (f ()))))
-
 (deftest test-mylast
+  (defn exec-last [f]
+    (is (= 8 (f '(1, 1, 2, 3, 5, 8))))
+    (is (nil? (f ()))))
   (exec-last mylast)
   (exec-last mylast2))
 
@@ -15,5 +14,9 @@
     (is (nil? (f ()))))
   (exec penultimate)
   (exec penultimate2))
+
+(deftest test-mylength
+  (is (= 6 (mylength '(1, 1, 2, 3, 5, 8))))
+  (is (= 0 (mylength '()))))
 
 (run-tests)
