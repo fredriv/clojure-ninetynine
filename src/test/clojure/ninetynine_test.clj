8bd15fb4f7e1783751c2b4550bf181561f8e4d6f
Alf Kristian Stoyle <alf.kristian@gmail.com>
2010-03-16 23:41:21 +0100

    Testing more mylast2 also.

--- src/test/clojure/ninetynine_test.clj
+++ src/test/clojure/ninetynine_test.clj
@@ -1,8 +1,13 @@
 (ns ninetynine_test
   (:use ninetynine clojure.test))
 
+(defn exec-last [f]
+  (is (= 8 (f '(1, 1, 2, 3, 5, 8))))
+  (is (nil? (f ()))))
+
 (deftest test-mylast
-  (is (= 8 (mylast '(1, 1, 2, 3, 5, 8))))
-  (is (nil? (mylast ()))))
+  (exec-last mylast)
+  (exec-last mylast2))
+
 
 (run-tests)
