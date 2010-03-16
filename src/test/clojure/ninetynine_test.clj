060a00146f027826cf78c74cc4748c3c2991f5f1
Alf Kristian Stoyle <alf.kristian@gmail.com>
2010-03-16 23:49:41 +0100

    Test penultimate

--- src/test/clojure/ninetynine_test.clj
+++ src/test/clojure/ninetynine_test.clj
@@ -1,7 +1,7 @@
 (ns ninetynine_test
   (:use ninetynine clojure.test))
 
-(defn exec-last [f]
+(defn- exec-last [f]
   (is (= 8 (f '(1, 1, 2, 3, 5, 8))))
   (is (nil? (f ()))))
 
@@ -9,5 +9,11 @@
   (exec-last mylast)
   (exec-last mylast2))
 
+(deftest test-penultimate
+  (defn exec [f]
+    (is (= 5 (f '(1, 1, 2, 3, 5, 8))))
+    (is (nil? (f ()))))
+  (exec penultimate)
+  (exec penultimate2))
 
 (run-tests)
